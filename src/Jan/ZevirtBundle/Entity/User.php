2dce3bed7be6faf7686afdcf96783c849b1c2005
jakob <jakob@tower>
2013-06-26 08:28:06 +0200

    user must extend fos model

--- src/Jan/ZevirtBundle/Entity/User.php
+++ src/Jan/ZevirtBundle/Entity/User.php
@@ -3,7 +3,7 @@
 namespace Jan\ZevirtBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use FOS\UserBundle\Entity\User as BaseUser;
+use FOS\UserBundle\Model\User as BaseUser;
 use FR3D\LdapBundle\Model\LdapUserInterface;
 
 /**
